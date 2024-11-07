dbaea818b8add1dbcc19e0c4feac132d2010fbce
ReidHomola <reidghomola@gmail.com>
2024-11-07 00:05:29 -0600

    added header, in process of adding nav bar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,65 @@
+import { useRef } from "react";
+
+type NavItem = {
+    label: string;
+    link: string;
+    className: string;
+    ref?: React.MutableRefObject<HTMLAnchorElement | null>;
+};
+
+interface Props {
+    navOpen: boolean;
+}
+
+function Navbar({ navOpen }: Props) {
+    const lastActiveLink = useRef<HTMLAnchorElement | null>(null);
+    const activeBox = useRef<HTMLDivElement | null>(null);
+
+    const navItems: NavItem[] = [
+        {
+            label: "Home",
+            link: "#home",
+            className: "nav-link active",
+            ref: lastActiveLink,
+        },
+        {
+            label: "About",
+            link: "#about",
+            className: "nav-link",
+        },
+        {
+            label: "Work",
+            link: "#work",
+            className: "nav-link",
+        },
+        {
+            label: "Reviews",
+            link: "#reviews",
+            className: "nav-link",
+        },
+        {
+            label: "Contact",
+            link: "#contact",
+            className: "nav-link md:hidden",
+        },
+    ];
+
+    return (
+        <nav className={"navbar " + (navOpen ? "active" : "")}>
+            {navItems.map(({ label, link, className, ref }, key) => (
+                <a
+                    href={link}
+                    key={key}
+                    ref={ref}
+                    className={className}
+                    onClick={null}
+                >
+                    {label}
+                </a>
+            ))}
+            <div className="active-box" ref={activeBox}></div>
+        </nav>
+    );
+}
+
+export default Navbar;
