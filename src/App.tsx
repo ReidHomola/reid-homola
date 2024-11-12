2f85f90d0ae4f5f48dc7a0366981be365083489f
ReidHomola <reidghomola@gmail.com>
2024-11-12 00:14:57 -0600

    Added about page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import About from "./components/About";
 
 const App = () => {
-    return (
-        <>
-            <Header />;
-            <main>
-                <Hero />
-            </main>
-        </>
-    );
+  return (
+    <>
+      <Header />;
+      <main>
+        <Hero />
+        <About />
+      </main>
+    </>
+  );
 };
 
 export default App;
