4150727bf7cd43006ea15b6d61d712e3d13a4e56
ReidHomola <reidghomola@gmail.com>
2024-11-12 23:04:38 -0600

    Added Projects

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Review from "./components/Review";
 
 const App = () => {
   return (
@@ -11,6 +13,8 @@ const App = () => {
         <Hero />
         <About />
         <Skills />
+        <Projects />
+        <Review />
       </main>
     </>
   );
