74209935bcf53d7c1761c9378bcac2a36c9c88e8
ReidHomola <reidghomola@gmail.com>
2024-11-14 23:51:50 -0600

    Updated skills and about content

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,11 +1,11 @@
 const aboutItems = [
   {
-    label: "Project done",
+    label: "Projects done",
     number: 45,
   },
   {
     label: "Years of experience",
-    number: 10,
+    number: 3,
   },
 ];
 
@@ -15,11 +15,11 @@ function About() {
       <div className="container">
         <div className="bg-zinc-800/50 p-7 rounded-2xl md:p-12 reveal-up">
           <p className="text-zinc-300 mb-4 md:mb-8 md:text-xl md:max-w-[60ch]">
-            Welcome! I&apos;m Reid, a professional web developer with a knack
-            for crafting visually stunning and highly functional websites.
-            Combining creativity and technical expertise, I transform your
-            vision into a digital masterpiece that excels in both appearance and
-            performance.
+            <strong>Welcome!</strong> I&apos;m Reid, a dedicated software
+            developer specializing in creating elegant and efficient digital
+            solutions. Whether it&apos;s designing intuitive interfaces or
+            building scalable backends, I&apos;m committed to delivering
+            exceptional results.
           </p>
           <div className="flex flex-wrap items-center gap-4 md:gap-7">
             {aboutItems.map(({ label, number }, key) => (
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -2,34 +2,9 @@ import SkillCard from "./SkillCard";
 
 const skillItems = [
   {
-    imgSrc: "/images/figma.svg",
-    label: "Figma",
-    desc: "Design tool",
-  },
-  {
-    imgSrc: "/images/css3.svg",
-    label: "CSS",
-    desc: "User Interface",
-  },
-  {
-    imgSrc: "/images/javascript.svg",
-    label: "JavaScript",
-    desc: "Interaction",
-  },
-  {
-    imgSrc: "/images/nodejs.svg",
-    label: "NodeJS",
-    desc: "Web Server",
-  },
-  {
-    imgSrc: "/images/expressjs.svg",
-    label: "ExpressJS",
-    desc: "Node Framework",
-  },
-  {
-    imgSrc: "/images/mongodb.svg",
-    label: "MongoDB",
-    desc: "Database",
+    imgSrc: "/images/csharp.svg",
+    label: "C# and .NET",
+    desc: "Backend Programming",
   },
   {
     imgSrc: "/images/react.svg",
@@ -41,17 +16,62 @@ const skillItems = [
     label: "TailwindCSS",
     desc: "User Interface",
   },
+  {
+    imgSrc: "/images/python.svg",
+    label: "Python",
+    desc: "Scripting",
+  },
+  {
+    imgSrc: "/images/microsoft-azure.svg",
+    label: "Azure",
+    desc: "Cloud Services",
+  },
+  {
+    imgSrc: "/images/sql.svg",
+    label: "SQL",
+    desc: "Relational Database",
+  },
+  {
+    imgSrc: "/images/mongodb.svg",
+    label: "MongoDB",
+    desc: "Database",
+  },
+  {
+    imgSrc: "/images/typescript.svg",
+    label: "TypeScript",
+    desc: "Web Interaction",
+  },
+  {
+    imgSrc: "/images/power-apps.svg",
+    label: "Power Apps",
+    desc: "Applications",
+  },
+  {
+    imgSrc: "/images/power-bi.svg",
+    label: "Power BI",
+    desc: "Analytics & Reporting",
+  },
+  {
+    imgSrc: "/images/azure-functions.svg",
+    label: "Azure Functions",
+    desc: "Cloud APIs",
+  },
+  {
+    imgSrc: "/images/power-automate.svg",
+    label: "Power Automate",
+    desc: "Automation",
+  },
 ];
 
 function Skills() {
   return (
     <section className="section">
       <div className="container">
-        <h2 className="headline-2 reveal-up">Essential Tools I Use</h2>
+        <h2 className="headline-2 reveal-up">Essential tools I use</h2>
 
         <p className="text-zinc-400 mt-3 mb-8 max-w-[50ch] reveal-up">
           Discover the powerful tools and technologies I use to create
-          exceptional, high-performing websites & applications.
+          exceptional, high-performing websites, automation, and applications.
         </p>
 
         <div className="grid gap-3 grid-cols-[repeat(auto-fill,_minmax(250px,_1fr))]">
